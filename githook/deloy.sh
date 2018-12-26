a963f798714892d48a92e75fb0ecb46fe1a18560
panzer-vor <xyk0279@gmail.com>
2018-12-26 15:57:48 +0800

    test

--- githook/deloy.sh
+++ githook/deloy.sh
@@ -1,5 +1,5 @@
 echo "Start deployment"
-cd $WEB_PATH
+# cd $WEB_PATH
 echo "pulling source code..."
 # git reset --hard origin/master
 # git clean -f
--- githook/index.js
+++ githook/index.js
@@ -21,7 +21,7 @@ http.createServer(function (req, res) {
 handler.on('error', function (err) {
   console.error('Error:', err.message)
 })  
- 
+  
 handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
