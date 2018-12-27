6ca0310fae0fbffacc513dcf739ebaa2261e5752
panzer-vor <xyk0279@gmail.com>
2018-12-27 15:28:16 +0800

    t

---
+++ admin/config/pro.config.js
@@ -0,0 +1,8 @@
+const proConfig = {
+  httpConfig: {
+    baseUri: 'http://localhost:7001/api',
+  },
+  env: "production",
+}
+
+module.exports = JSON.stringify(proConfig)
--- admin/src/config/globalConfig.ts
+++ admin/src/config/globalConfig.ts
@@ -1,5 +1,6 @@
-
 const config: string = process.env.NODE_ENV || ''
-console.log(config);
-const globalConfig = JSON.parse(config)
+console.log(config)
+const globalConfig = JSON.parse(config) || {
+  httpConfig: {}
+}
 export const { httpConfig } = globalConfig
